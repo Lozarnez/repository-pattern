83e30b3516613dbf427bc4a2d13f81d36ce33f57
Juan Pablo Loaiza <jploaiza@dicalzeus.com>
2024-06-26 14:23:02 -0700

    fix: modificación de test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleNameMapper: {
     '^@db/(.*)$': '<rootDir>/src/app/shared/db/$1',
     '^@lib$': '<rootDir>/src/app/shared/lib',
+    '^@schema$': '<rootDir>/src/app/shared/schema',
     '^@service/(.*)$': '<rootDir>/src/app/shared/service/$1',
     '^@repository/(.*)$': '<rootDir>/src/app/shared/repository/$1',
   },
